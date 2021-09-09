4eb4de6a49ee3788b3aa5d19d951eb2426481eb5
EugeneBatr <bavtruskozena@gmail.com>
2021-09-09 15:24:47 +0300

    Save

---
+++ homework2.js
@@ -0,0 +1,88 @@
+////////////////////////1///////////////////////////
+
+// numArray = [1, 2, 2, 2, 3, 4, 5, 5, 7];
+
+// numArray = [1, 1, 2];
+
+// const filterUnique = numArray.filter((number, index) => {
+
+//         if (typeof number === 'number') return numArray.indexOf(number) === index;
+// })
+
+// console.log(filterUnique);
+
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////2///////////////////////////
+
+// numArray = [1, 2, 3];
+
+// numArray = [1, 1, 2];
+
+// const cubizer = numArray.map((number) => {
+//         if (typeof number === 'number') return number ** 3;
+// })
+
+// console.log(cubizer);
+
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////3///////////////////////////
+
+// // array = ['a', 'b', 'dsd'];
+
+// array = ['hello','hi','JavaScript','efghe']
+
+// const filterLongString = (array, number) => array.filter((string) => {
+//         if (typeof string === 'string') return string.length ===  number;
+        
+// })
+
+// console.log(filterLongString(array, 5));
+
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////4///////////////////////////
+
+// array = ['a', 1, 'b', 2, 'sqw', [1,2,3]];
+
+// array = ['Hello', 'World', 'sqw', [1,2,3]];
+
+// const filterOnlyNumbers = array.filter((item) => {
+//         if (typeof item === 'number') return item;
+// })
+
+// console.log(filterOnlyNumbers);
+
+/////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////5///////////////////////////
+
+// numArray = [1,3,5,2];
+
+// numArray = [3,5,2134,65,4456,10]
+
+// const findMaxNumber = numArray.reduce((result, number) => {
+//         if (typeof number === 'number' )
+//         return result > number ? result : number; 
+// })
+
+// console.log(findMaxNumber)
+
+/////////////////////////////////////////////////////
+
